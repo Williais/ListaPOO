14248722ce922ac53150ff50bf1bec6a93dab806
Willian Padilha <45665895+Williais@users.noreply.github.com>
2026-04-03 00:40:13 -0300

    questão 1

--- Lista7/Palavra.java
+++ Lista7/Palavra.java
@@ -1,4 +1,19 @@
 package Lista7;
 
 public class Palavra {
+    private String palavra;
+    private String dica;
+
+    public Palavra(String p, String d){
+        this.palavra = p;
+        this.dica = d;
+    }
+
+    public void setPalavra(String p){
+        this.palavra = p;
+    }
+
+    public String getPalavra(){
+        return palavra;
+    }
 }
