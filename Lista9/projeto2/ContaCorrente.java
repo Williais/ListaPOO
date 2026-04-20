9b4c69e55c97d4be449ec15a0df7be89826c2039
Willian Padilha <45665895+Williais@users.noreply.github.com>
2026-04-20 00:13:58 -0300

    Atualizando a Lista 9 - Questão 5

--- Lista9/projeto2/ContaCorrente.java
+++ Lista9/projeto2/ContaCorrente.java
@@ -1,4 +1,52 @@
 package Lista9.projeto2;
 
 public class ContaCorrente {
+
+    private float saldo;
+    private String titular;
+    private String numero;
+    private float chequeEspecial;
+
+    public ContaCorrente(float saldo, String titular, String numero, float chequeEspecial) {
+        this.saldo = saldo;
+        this.titular = titular;
+        this.numero = numero;
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public float getChequeEspecial() {
+        return chequeEspecial;
+    }
+
+    public void setChequeEspecial(float chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
+
+    public String toString(){
+        return "Tipo da Conta: Conta Corrente - Numero da Conta: " + numero + " Com Saldo Total: R$ " + saldo;
+    }
 }
--- Lista9/projeto2/ContaPoupanca.java
+++ Lista9/projeto2/ContaPoupanca.java
@@ -1,4 +1,52 @@
 package Lista9.projeto2;
 
 public class ContaPoupanca {
+
+    private float saldo;
+    private String titular;
+    private String numero;
+    private float taxaDeRendimento;
+
+    public ContaPoupanca(float saldo, String titular, String numero, float taxaDeRendimento) {
+        this.saldo = saldo;
+        this.titular = titular;
+        this.numero = numero;
+        this.taxaDeRendimento = taxaDeRendimento;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public float getTaxaDeRendimento() {
+        return taxaDeRendimento;
+    }
+
+    public void setTaxaDeRendimento(float taxaDeRendimento) {
+        this.taxaDeRendimento = taxaDeRendimento;
+    }
+
+    public String toString(){
+        return "Tipo da Conta: Conta Poupança - Numero da Conta: " + numero + " Com Saldo Total: R$ " + saldo;
+    }
 }
