31ac7d0d5b2a244426ce480409308bd3c9f02973
Willian Padilha <45665895+Williais@users.noreply.github.com>
2026-05-23 00:43:35 -0300

    Iniciando a lista 13

---
+++ Lista13/LanceDado.java
@@ -0,0 +1,15 @@
+package Lista13;
+
+import javax.swing.*;
+
+public class LanceDado extends JFrame {
+
+    public LanceDado(){
+        setTitle("Sortear Dado");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(600, 900);
+
+
+    }
+}
